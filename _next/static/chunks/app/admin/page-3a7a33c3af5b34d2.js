dc014697aef4577cba9d9c7e81b1033c80d0bcd3
Barcelona Games Deploy <deploy@barcelonagames.local>
2026-09-18 21:46:14 +0200

    Fix: restore .nojekyll (GitHub Pages was excluding _next/ and breaking all CSS/JS)

--- _next/static/chunks/app/admin/page-3a7a33c3af5b34d2.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3,160,499,88,953,423,11,89],{2596:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=2596)}),_N_E=n.O()}]);
