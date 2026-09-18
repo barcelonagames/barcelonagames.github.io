40ec78eb2511c194c79f8d0fe1b821ce2f0b4378
Barcelona Games Deploy <deploy@barcelonagames.local>
2026-09-18 22:27:33 +0200

    Site-wide cleanup: trim nav, fix pricing/links, simplify Instagram icon, swap lounge photo, merge layout, lazy-load images + 3D trophy

--- _next/static/chunks/app/not-found-a037072a27cbfd9c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160,3,423,11,89,88,953,499],{2596:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=2596)}),_N_E=n.O()}]);
