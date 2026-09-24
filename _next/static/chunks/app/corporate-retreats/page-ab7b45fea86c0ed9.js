ce72f04661a3bda4637d078925f026876668804e
Barcelona Games Deploy <deploy@barcelonagames.local>
2026-09-24 16:19:34 +0200

    Deploy 2026-09-24 16:19

---
+++ _next/static/chunks/app/corporate-retreats/page-ab7b45fea86c0ed9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[174],{6881:function(e,n,s){Promise.resolve().then(s.bind(s,3212)),Promise.resolve().then(s.bind(s,6802))}},function(e){e.O(0,[523,971,23,744],function(){return e(e.s=6881)}),_N_E=e.O()}]);
